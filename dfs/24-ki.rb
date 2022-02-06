911f8c14982226d7f65e5b88fc56edda25aebaa7
Aoyama Taro <a11111111@aoyama.jp>
2022-02-06 12:05:11 +0900

    finish 24 ki

--- dfs/24-ki.rb
+++ dfs/24-ki.rb
@@ -1,16 +1,33 @@
-N, Q = gets.split.map &:to_i
 
-nodes = Array.new(N) { [] }
+=begin 
+  point
+  - dfsのやり方、親に行かない方法
+  - 
+=end
+# AC回答
+N, Q = gets.split.map &:to_i
+$to = Array.new(N) { [] }
+$cnts = Array.new(N) { 0 }
+
+def dfs(v, pp=-1) # 今見ている頂点（部分木の親, pp：どこからきたのか
+  ajs = $to[v]
+  ajs.each do |u|
+    next if u == pp # 親に行こうとしたら飛ばす
+    $cnts[u] += $cnts[v]
+    dfs(u, v)
+  end
+end
+
 (N-1).times do |i|
   a, b = gets.split.map(&:to_i).map { |x| x -= 1 }
-  nodes[a] << b
-  nodes[b] << a
+  $to[a] << b
+  $to[b] << a
 end
 
-cnts = Array.new(N) { 0 }
 Q.times do |j|
   pp, x = gets.split.map(&:to_i)
-  
-
+  pp -= 1
+  $cnts[pp] += x
 end
-# puts cnts.jon(" ")
+dfs(0)
+puts $cnts.join(" ")
